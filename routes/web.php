57c90fe14092fcc7d3acfe95ed8127d2be2bcfc2
Patricija Legataitė <p.legataite@gmail.com>
2025-10-09 15:48:06 +0300

    Practising responses, cookies

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use PharIo\Manifest\Author;
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/', static function () {
     return 'Welcome to the Home page';
@@ -79,4 +82,71 @@ Route::prefix('posts')->name('posts.')->group(function () use ($posts) {
         abort_if(!isset($posts[$postId]['authors'][$authorId]), 404);
         return view('posts.author', ['post' => $posts[$postId], 'author' => $posts[$postId]['authors'][$authorId]]);
     })->name('author');
+});
+
+$users = [
+    2 => [
+        'id' => 2,
+        'name' => 'Petras',
+        'surname' => 'Petraitis',
+        'birthday' => '2013-10-12',
+        'cart' => [
+            '0001' => [
+                'keyword' => 'leather_jacket',
+                'color' => 'black',
+            ],
+            '0053' => [
+                'keyword' => 'jeans',
+                'color' => 'blue',
+            ],
+            '0063' => [
+                'keyword' => 'hat',
+                'color' => 'purple',
+            ],
+        ]
+    ],
+    35 => [
+        'id' => 35,
+        'name' => 'Jonas',
+        'surname' => 'Jonaitis',
+        'birthday' => '2005-04-15',
+        'cart' => [
+            '0001' => [
+                'keyword' => 'leather_jacket',
+                'color' => 'black',
+            ],
+            '0053' => [
+                'keyword' => 'jeans',
+                'color' => 'blue',
+            ],
+            '0063' => [
+                'keyword' => 'hat',
+                'color' => 'purple',
+            ],
+        ]
+    ],
+];
+
+Route::get('/', function () {
+    return 'Welcome to the Home page';
+})->name('dashboard');
+
+route::prefix('/user')->name('user.')->group(function () use ($users) {
+    Route::get('{id}', function ($id) use ($users) {
+        if (Carbon::parse($users[$id]['birthday'])->age > 13) {
+            return redirect()->route('dashboard');
+        }
+        abort(403);
+    });
+
+    Route::get('{id}/cart', function ($id) use ($users) {
+        return response('success', 200)
+            ->cookie('USER_CART', json_encode($users[$id]['cart']), 3600);
+    })->name('cart');
+
+    Route::get('list/filter', function (Request $request) {
+        return response()->json($request->query());
+        // http://127.0.0.1:8000/user/list/filter?limit=10&page=2
+        // {"limit":"10","page":"2"}
+    });
 });
